914ed0fcabf0c4088c987034ac38d0304d9f4a3d
Jose Escobar <joanesco@espol.edu.ec>
2018-11-29 20:26:47 -0500

    Buscador semiterminado

--- Pagina Web/js/busqueda.js
+++ Pagina Web/js/busqueda.js
@@ -6,13 +6,15 @@ function agregarNoticia(titulo, contenido){
 	var title = $("<h5/>", {
 	  "class": "col col-12 mt-3",
 	  html: $("<h4/>",{
+		"class": "mont",
 	  	html:titulo
 	  })
 	});
 
 	var p = $("<div/>",{
-	  "class": "col col-12",
+	  "class": "col col-12 col-md-8",
 	  html: $("<p/>",{
+		"class":"rale",
 	  	html:contenido
 	  })
 	});
@@ -28,23 +30,31 @@ function agregarNoticia(titulo, contenido){
 
 function cargarNoticias(texto) {
 	"use strict";
+	var count=0;
 	$.ajax({
 		type: "GET",
 		url: "../data/noticias.xml",
 		dataType: "xml",
 		success: function(xml){
-			$(xml).find('noticia').each(function(){
-			var titulo = $(this).find('titulo').text();
-			var contenido = $(this).find('contenido').text();
 			if(texto.length===0){
-				agregarNoticia(titulo, contenido);
+				$("#noticias").text("No hay resultados");
 			}
 			else{
+				$(xml).find('noticia').each(function(){
+				var titulo = $(this).find('titulo').text();
+				var contenido = $(this).find('contenido').text();
+				
 				if(titulo.includes(texto) || contenido.includes(texto)){
 					agregarNoticia(titulo, contenido);
+					count++;
 				}
+				
+				});
 			}
-			});
+			
+			if (count===0){
+				$("#noticias").text("No hay resultados");
+			}
 		},
 		error: function() {
 		alert("Error al procesar el xml");
@@ -56,13 +66,11 @@ function datoBusqueda(){
 	"use strict";
 	var parameters=location.search.substring(1).split("&");
 	var temp=parameters[0].split("=");
-	var l=temp[1];
-	return l;
+	return temp[1];
 }
 
 $(document).ready(function(){
 	"use strict";
-	
 	var texto=datoBusqueda().toString();
 	cargarNoticias(texto);
 });
