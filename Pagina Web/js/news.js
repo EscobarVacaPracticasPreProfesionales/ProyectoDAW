42eb6831d5e627cc2b9406589e18b4eb69612782
Fernando Vaca <fdvaca@espol.edu.ec>
2018-11-30 04:55:58 -0500

    Grafico y correcion de error en xml

--- Pagina Web/js/news.js
+++ Pagina Web/js/news.js
@@ -24,3 +24,24 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 768px)")
 myFunction(x) 
 x.addListener(myFunction) 
+
+new Chart(document.getElementById("graficoBarras"), {
+    type: 'bar',
+    data: {
+      labels: ["2007", "2011", "2013", "2015", "2018"],
+      datasets: [
+        {
+          label: "Número de proyectos realizados por año",
+          backgroundColor: ["#5CB1F7","#5CB1F7","#5CB1F7","#5CB1F7","#5CB1F7"],
+          data: [1,3,3,4,3]
+        }
+      ]
+    },
+    options: {
+      legend: { display: true },
+      title: {
+        display: true,
+        text: 'Proyectos realizados por año'
+      }
+    }
+});
